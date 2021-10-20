bd0ed7d849fb1c0a39c792166c8ac3bc347435fa
jorkop <jorgen.kopperud@hotmail.com>
2021-10-20 20:43:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mappe2s344183s303045/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mappe2s344183s303045;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
